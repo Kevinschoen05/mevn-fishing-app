d96bfc40d1831d97ea811cc24e3163eee53e325d
Kevin Schoen <kevinschoen05@gmail.com>
2021-11-15 23:57:57 -0500

    Added form for submitting new records, notification on home page

--- client/src/views/AddRecord.vue
+++ client/src/views/AddRecord.vue
@@ -1,3 +1,102 @@
 <template>
-    <h1>Add Record Page</h1>
+  <v-container>
+    <v-row no-gutters>
+      <v-col sm="10" class="mx-auto">
+        <v-card class="pa-5">
+          <v-card-title>Record New Fish</v-card-title>
+          <v-divider></v-divider>
+          <v-form
+            ref="form"
+            @submit.prevent="submitForm"
+            class="pa-5"
+            enctype="multipart/form-data"
+          >
+            <v-text-field
+              label="Fish Species"
+              v-model="record.species"
+              prepend-icon="mdi-fish"
+              :rules="rules"
+            ></v-text-field>
+            <v-text-field
+              label="Name of Angler"
+              v-model="record.angler"
+              prepend-icon="mdi-account"
+              :rules="rules"
+            ></v-text-field>
+            <v-text-field
+              label="Weight of Fish"
+              v-model="record.weight"
+              prepend-icon="mdi-scale"
+              :rules="rules"
+            ></v-text-field>
+            <v-text-field
+              label="Reservoir"
+              v-model="record.reservoir"
+              prepend-icon="mdi-map"
+              :rules="rules"
+            ></v-text-field>
+            <v-file-input
+              @change="selectFile"
+              :rules="rules"
+              show-size
+              counter
+              multiple
+              label="Select Image"
+            ></v-file-input>
+            <v-textarea
+              label="Comments"
+              v-model="record.comment"
+              prepend-icon="mdi-note"
+              :rules="rules"
+            ></v-textarea>
+            <v-btn type="submit" class="mt-3" color="primary">Add Fish</v-btn>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<script>
+import API from "../api";
+export default {
+  data() {
+    return {
+      rules: [(value) => !!value || "This field is required"],
+      record: {
+        species: "",
+        angler: "",
+        weight: "",
+        reservoir: "",
+        comment: "",
+        date: "",
+        image: "",
+      },
+      image: "",
+    };
+  },
+  methods: {
+    selectFile(file) {
+      this.image = file[0];
+    },
+
+    async submitForm() {
+      const formData = new FormData();
+      formData.append("image", this.image);
+      formData.append("species", this.record.species);
+      formData.append("angler", this.record.angler);
+      formData.append("weight", this.record.weight);
+      formData.append("reservoir", this.record.reservoir);
+      formData.append("comment", this.record.comment);
+      console.log(formData);
+      if (this.$refs.form.validate()) {
+        const response = await API.addRecord(formData);
+        this.$router.push({
+          name: "Home",
+          params: { message: response.message },
+        });
+      }
+        console.log(formData);
+    },
+  },
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-  <v-container fill-height  >
+  <v-container fill-height>
+    <v-alert
+      border="left"
+      close-text="Close Alert"
+      color="green accent-4"
+      dismissible
+      v-if="this.$route.params.message"
+    >{{this.$route.params.message}}</v-alert>
     <v-row no-gutters class="d-flex justify-space-between">
       <v-col
         sm="4"
@@ -7,7 +14,7 @@
         v-for="reservoir in reservoirs"
         :key="reservoir.name"
       >
-        <v-card class="pa-1 " :to="reservoir.link">
+        <v-card class="pa-1" :to="reservoir.link">
           <v-img height="250" :src="reservoir.image"> </v-img>
           <v-card-title class="headline">{{ reservoir.name }}</v-card-title>
         </v-card>
@@ -17,8 +24,8 @@
 </template>
 
 <script>
-import crotonImage from "../assets/crotonThumbnail.png"
-import muscootImage from "../assets/muscootThumbnail.png"
+import crotonImage from "../assets/crotonThumbnail.png";
+import muscootImage from "../assets/muscootThumbnail.png";
 export default {
   name: "Home",
   data() {
@@ -27,12 +34,12 @@ export default {
         {
           name: "Muscoot",
           image: muscootImage,
-          link: '/muscoot'
+          link: "/muscoot",
         },
         {
           name: "Croton",
           image: crotonImage,
-          link: '/croton'
+          link: "/croton",
         },
       ],
     };
