bf149dde8b2869598e2878abf5f27048d89115b9
Kevin Schoen <kevinschoen05@gmail.com>
2021-12-01 00:23:04 -0500

    logic to get each species caught by each angler

--- client/src/components/AnglerCard.vue
+++ client/src/components/AnglerCard.vue
@@ -2,11 +2,31 @@
   <v-container>
     <v-card>
       <v-card-title>{{angler}}</v-card-title>
+        <h2 v-for=" species in speciesList" :key="species">{{species}}</h2>
     </v-card>
   </v-container>
 </template>
 <script>
 export default {
   props: ["records", "angler"],
+  data() {
+      return {
+          speciesList: [],
+      }
+  },
+  methods: {
+      getSpeciesList(angler){
+          var fishList =[];
+          for (var i =0; i < this.records.length; i++ ){
+              if(angler === this.records[i].angler){
+                  fishList.push(this.records[i].species)
+              }
+          }
+          this.speciesList =[...new Set(fishList)]
+      }
+  },
+  async created(){
+      this.getSpeciesList(this.angler)
+  }
 };
 </script>
